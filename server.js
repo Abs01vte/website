45c0438211ece7880297ef92868c518bc2755a50
kyle <hammykyle@gmail.com>
2022-01-18 03:45:42 -0700

    fixed access bug

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.get('/', (req, res, next) => {
   res.status(200).render('index');
 });
-app.get('/:writing', (req, res, next) => {
+app.get('/writings/:writing', (req, res, next) => {
   let writing = req.params.writing.toLowerCase();
   console.log(writing);
   res.status(200).render('writings',{text:getTextLines(writing)});
